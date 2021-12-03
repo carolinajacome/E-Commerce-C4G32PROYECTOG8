1b83ecf1c322ad8757a75c44a4bc6f53d2f561d0
Cristian Hernandez <christianhb534@gmail.com>
2021-12-02 22:21:55 -0500

    Imports App.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,27 @@ import Footer from "./components/layout/Footer";
 import Home from './components/Home';
 import ProductDetails from './components/product/ProductDetails';
 
+//Cart Imports
+import Cart from './components/cart/Cart'
+import Shipping from './components/cart/Shipping'
+import ConfirmOrder from './components/cart/ConfirmOrder'
+import Payment from './components/cart/Payment'
+import OrderSuccess from './components/cart/OrderSuccess'
+
+//Order Imports
+import ListOrders from './components/order/ListOrders'
+import OrderDetails from './components/order/OrderDetails'
+
+//Auth or User Imports
+import Login from './components/user/Login'
+import Register from './components/user/Register'
+import Profile from './components/user/Profile'
+import UpdateProfile from './components/user/UpdateProfile'
+import UpdatePassword from './components/user/UpdatePassword'
+import ForgotPassword from './components/user/ForgotPassword'
+import NewPassword from './components/user/NewPassword'
+
+
 // Admin Imports
 import Dashboard from './components/admin/Dashboard'
 import ProductsList from "./components/admin/ProductsList";
@@ -19,6 +40,11 @@ import UsersList from "./components/admin/UsersList";
 import UpdateUser from "./components/admin/UpdateUser";
 import ProductReviews from "./components/admin/ProductReviews";
 
+import ProtectedRoute from './components/route/ProtectedRoute'
+import { loadUser } from './actions/userActions'
+import store from './store'
+import axios from 'axios'
+
 import { useSelector } from 'react-redux'
 
 function App() {
@@ -29,14 +55,20 @@ function App() {
   return (
     <Router>
       <div className="App">
+
         <Header />
+
         <div className="container container-fluid my-5">
           <Route path="/" component={Home} exact />
           <Route path="/product/:id" component={ProductDetails} exact />
         </div>
+
         <ProtectedRoute path="/dashboard" isAdmin={true} component={Dashboard} exact />
+
         <ProtectedRoute path="/admin/products" isAdmin={true} component={ProductsList} exact />
+
         <ProtectedRoute path="/admin/product" isAdmin={true} component={NewProduct} exact />
+
         <ProtectedRoute path="/admin/product/:id" isAdmin={true} component={UpdateProduct} exact />
 
         <ProtectedRoute path="/admin/orders" isAdmin={true} component={OrdersList} exact />
